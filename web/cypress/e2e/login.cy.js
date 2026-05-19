80a58d6b2c8a16ae590d4796551b0dd7f7e8e7bf
Marcos Hendell <marcos-hendell@hotmail.com>
2026-05-19 11:28:03 -0300

    atualiza dependencias

--- web/cypress/e2e/login.cy.js
+++ web/cypress/e2e/login.cy.js
@@ -1,34 +1,23 @@
 import { getTodayDate } from "../support/utils"
 
-// Agrupa os testes relacionados à funcionalidade de Login
 describe('Login', () => {
-
-
-  // Define um caso de teste com o nome "Deve logar com sucesso"
+ 
   it('Deve logar com sucesso', () => {
-    // Executa um comando customizado chamado "submeterLogin"
-    // passando dois parâmetros: email e senha
+    
     cy.start()
     cy.submitLoginForm('papito@webdojo.com', 'katana123')
-
-    // Seleciona o elemento que possui o atributo data-cy="user-name"
     cy.get('[data-cy="user-name"]')
 
-      // Verifica se o elemento está visível na tela
-      .should('be.visible')
-
-      // Verifica se o texto exibido é exatamente "Fernando Papito"
+            .should('be.visible')
+    
       .and('have.text', 'Fernando Papito')
-
-    // Seleciona o elemento que possui o atributo data-cy="welcome-message"
+  
     cy.get('[data-cy="welcome-message"]')
 
-      // Verifica se o elemento está visível
       .should('be.visible')
-
-      // Verifica se o texto exibido é exatamente o esperado
+   
       .and('have.text', 'Olá QA, esse é o seu Dojo para aprender Automação de Testes.')
-////////////////////////////////////////////////////////////////////////////////
+  
     cy.getCookie('login_date').should((cookie) => {
       expect(cookie.value).to.eq(getTodayDate())
     })
@@ -39,7 +28,6 @@ describe('Login', () => {
     })
 
   })
-///////////////////////////////////////////////////////////////////////////////////
   it('Não deve logar com a senha inválida', () => {
     cy.start()
     cy.submitLoginForm('papito@webdojo.com', 'atana123')
